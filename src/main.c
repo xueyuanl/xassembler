569038de6fde113172db61acb2c042ab510706a5
pi <15186846+xueyuanl@users.noreply.github.com>
2020-04-06 14:30:47 +0100

    add print result

--- src/main.c
+++ src/main.c
@@ -67,20 +67,23 @@ void BuildXSE() {
     // Write the ID string (4 bytes)
 
     fwrite(XSE_ID_STRING, 4, 1, pExecFile);
+    printf("XSE_ID: %s\n", XSE_ID_STRING);
     // Write the version (1 byte for each component, 2 total)
 
     char cVersionMajor = VERSION_MAJOR,
             cVersionMinor = VERSION_MINOR;
     fwrite(&cVersionMajor, 1, 1, pExecFile);
+    printf("Major Version: %d\n", cVersionMajor);
     fwrite(&cVersionMinor, 1, 1, pExecFile);
+    printf("Minor Version: %c\n", cVersionMinor);
 
     // Write the stack size (4 bytes)
-
     fwrite(&g_ScriptHeader.iStackSize, 4, 1, pExecFile);
+    printf("Stack Size: %d\n", g_ScriptHeader.iStackSize);
 
     // Write the global data size (4 bytes )
-
     fwrite(&g_ScriptHeader.iGlobalDataSize, 4, 1, pExecFile);
+    printf("Global Data Size: %d\n", g_ScriptHeader.iGlobalDataSize);
 
     // Write the _Main () flag (1 byte)
 
@@ -88,29 +91,29 @@ void BuildXSE() {
     if (g_ScriptHeader.iIsMainFuncPresent)
         cIsMainPresent = 1;
     fwrite(&cIsMainPresent, 1, 1, pExecFile);
+    printf("Main Func Present: %d\n", cIsMainPresent);
 
     // Write the _Main () function index (4 bytes)
-
     fwrite(&g_ScriptHeader.iMainFuncIndex, 4, 1, pExecFile);
+    printf("Main Func Index: %d\n", g_ScriptHeader.iMainFuncIndex);
 
     // ---- Write the instruction stream
 
     // Output the instruction count (4 bytes)
-
     fwrite(&g_iInstrStreamSize, 4, 1, pExecFile);
+    printf("Instr Stream Size: %d\n", g_iInstrStreamSize);
 
     // Loop through each instruction and write its data out
-
     for (int iCurrInstrIndex = 0; iCurrInstrIndex < g_iInstrStreamSize; ++iCurrInstrIndex) {
         // Write the opcode (2 bytes)
-
         short sOpcode = g_pInstrStream[iCurrInstrIndex].iOpCode;
         fwrite(&sOpcode, 2, 1, pExecFile);
+        printf("OpCode: %d, ", sOpcode);
 
         // Write the operand count (1 byte)
-
         char iOpCount = g_pInstrStream[iCurrInstrIndex].iOpCount;
         fwrite(&iOpCount, 1, 1, pExecFile);
+        printf("OpCount: %d, ", iOpCount);
 
         // Loop through the operand list and print each one out
 
@@ -120,41 +123,45 @@ void BuildXSE() {
             Operand CurrOp = g_pInstrStream[iCurrInstrIndex].pOpList[iCurrOpIndex];
 
             // Create a character for holding operand types (1 byte)
-
             char cOpType = CurrOp.iType;
             fwrite(&cOpType, 1, 1, pExecFile);
+            printf("OpType: %d, ", cOpType);
 
             // Write the operand depending on its type
-
             switch (CurrOp.iType) {
                 // Integer literal
 
                 case OP_TYPE_INT:
                     fwrite(&CurrOp.iIntLiteral, sizeof(int), 1, pExecFile);
+                    printf("Int value: %d ", CurrOp.iIntLiteral);
                     break;
 
                     // Floating-point literal
 
                 case OP_TYPE_FLOAT:
                     fwrite(&CurrOp.fFloatLiteral, sizeof(float), 1, pExecFile);
+                    printf("FLOAT value: %f ", CurrOp.fFloatLiteral);
                     break;
 
                     // String index
 
                 case OP_TYPE_STRING_INDEX:
                     fwrite(&CurrOp.iStringTableIndex, sizeof(int), 1, pExecFile);
+                    printf("Str Index: %d ", CurrOp.iStringTableIndex);
                     break;
 
                     // Instruction index
 
                 case OP_TYPE_INSTR_INDEX:
                     fwrite(&CurrOp.iInstrIndex, sizeof(int), 1, pExecFile);
+                    printf("Instr Index: %d ", CurrOp.iInstrIndex);
                     break;
 
                     // Absolute stack index
 
                 case OP_TYPE_ABS_STACK_INDEX:
                     fwrite(&CurrOp.iStackIndex, sizeof(int), 1, pExecFile);
+                    printf("Abs Stack Index: %d ", CurrOp.iStackIndex);
                     break;
 
                     // Relative stack index
@@ -162,27 +169,32 @@ void BuildXSE() {
                 case OP_TYPE_REL_STACK_INDEX:
                     fwrite(&CurrOp.iStackIndex, sizeof(int), 1, pExecFile);
                     fwrite(&CurrOp.iOffsetIndex, sizeof(int), 1, pExecFile);
+                    printf("rel Stack Index: %d ", CurrOp.iOffsetIndex);
                     break;
 
                     // Function index
 
                 case OP_TYPE_FUNC_INDEX:
                     fwrite(&CurrOp.iFuncIndex, sizeof(int), 1, pExecFile);
+                    printf("Func Index: %d ", CurrOp.iFuncIndex);
                     break;
 
                     // Host API call index
 
                 case OP_TYPE_HOST_API_CALL_INDEX:
                     fwrite(&CurrOp.iHostAPICallIndex, sizeof(int), 1, pExecFile);
+                    printf("Host API Call Index: %d ", CurrOp.iHostAPICallIndex);
                     break;
 
                     // Register
 
                 case OP_TYPE_REG:
                     fwrite(&CurrOp.iReg, sizeof(int), 1, pExecFile);
+                    printf("REG Index: %d ", CurrOp.iReg);
                     break;
             }
         }
+        printf("\n");
     }
 
     // Create a node pointer for traversing the lists
@@ -193,8 +205,8 @@ void BuildXSE() {
     // ---- Write the string table
 
     // Write out the string count (4 bytes)
-
     fwrite(&g_StringTable.iNodeCount, 4, 1, pExecFile);
+    printf("String count: %d\n", g_StringTable.iNodeCount);
 
     // Set the pointer to the head of the list
 
@@ -215,8 +227,9 @@ void BuildXSE() {
         // Write the length (4 bytes), followed by the string data (N bytes)
 
         fwrite(&iCurrStringLength, 4, 1, pExecFile);
+        printf("Str length: %d, ", iCurrStringLength);
         fwrite(pstrCurrString, strlen(pstrCurrString), 1, pExecFile);
-
+        printf("Str Value: %s\n", pstrCurrString);
         // Move to the next node
 
         pNode = pNode->pNext;
@@ -227,6 +240,7 @@ void BuildXSE() {
     // Write out the function count (4 bytes)
 
     fwrite(&g_FuncTable.iNodeCount, 4, 1, pExecFile);
+    printf("Func count: %d\n", g_FuncTable.iNodeCount);
 
     // Set the pointer to the head of the list
 
@@ -242,15 +256,18 @@ void BuildXSE() {
         // Write the entry point (4 bytes)
 
         fwrite(&pFunc->iEntryPoint, sizeof(int), 1, pExecFile);
+        printf("Entry Point: %d, ", pFunc->iEntryPoint);
 
         // Write the parameter count (1 byte)
 
         cParamCount = pFunc->iParamCount;
         fwrite(&cParamCount, 1, 1, pExecFile);
+        printf("Param Count: %d, ", cParamCount);
 
         // Write the local data size (4 bytes)
 
         fwrite(&pFunc->iLocalDataSize, sizeof(int), 1, pExecFile);
+        printf("Local Data Size: %d\n", pFunc->iLocalDataSize);
 
         // Move to the next node
 
@@ -262,6 +279,7 @@ void BuildXSE() {
     // Write out the call count (4 bytes)
 
     fwrite(&g_HostAPICallTable.iNodeCount, 4, 1, pExecFile);
+    printf("Host API Call count: %d\n", g_HostAPICallTable.iNodeCount);
 
     // Set the pointer to the head of the list
 
@@ -278,7 +296,9 @@ void BuildXSE() {
         // Write the length (1 byte), followed by the string data (N bytes)
 
         fwrite(&cCurrHostAPICallLength, 1, 1, pExecFile);
+        printf("Host API Call Length: %d, ", cCurrHostAPICallLength);
         fwrite(pstrCurrHostAPICall, strlen(pstrCurrHostAPICall), 1, pExecFile);
+        printf("Host API Call is: %s\n", pstrCurrHostAPICall);
 
         // Move to the next node
 
@@ -379,9 +399,9 @@ int main(int argc, char *argv[]) {
 ////    }*/
 
     Init();
-    g_iSourceFileLine = LoadSourceFile("script");
+    g_iSourceFileLine = LoadSourceFile("/Users/hsuehyuan/mnt/pi/assembler/src/script");
     parse();
-    PrintTableInfo();
+    //PrintTableInfo();
     BuildXSE();
 
     ShutDown();
